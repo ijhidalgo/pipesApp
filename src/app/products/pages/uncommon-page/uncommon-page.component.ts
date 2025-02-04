63cf3109ded2df44fac7cda939e757006d4fc6b4
Ivan <ijhidalgo@minsait.com>
2025-02-04 17:04:23 +0100

    Fin pipes Angular

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -21,4 +22,32 @@ export class UncommonPageComponent {
     this.name = 'Melissa';
     this.gender = 'female';
   }
+
+  // i18nPlural
+  public clients:string[] = ['Maria', 'Pedro', 'Fernando', 'Juan', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos #  clientes esperando.'
+  }
+  deleteClient():void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+  }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000);
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+      resolve('Tenemos data en la promesa.');
+      this.person.name = 'Otro nombre';
+    }, 3500);
+  })
 }
